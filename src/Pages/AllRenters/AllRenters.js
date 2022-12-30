1186a9f2c990e55cced2e3f6421435f9e95508b8
RaiAn-CSE <68633815+RaiAn-CSE@users.noreply.github.com>
2022-12-30 23:47:37 +0600

    dashboard

---
+++ src/Pages/AllRenters/AllRenters.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import UserList from '../../component/OwnerList/OwnerList';
+
+const AllRenters = () => {
+    return (
+        <div>
+            <h2>This Is all Renters</h2>
+            <UserList></UserList>
+        </div>
+    );
+};
+
+export default AllRenters;
---
+++ src/Pages/Dashboard/Dashboard.js
@@ -0,0 +1,43 @@
+import React, { useContext, useEffect, useState } from "react";
+import { Link, Outlet } from "react-router-dom";
+// import MyProperty from "../../component/MyProperty/MyProperty";
+import { AuthContext } from '../../contexts/AuthProvider';
+import useAdmin from '../../hooks/useAdmin';
+import MyProperty from "../../component/MyProperty/MyProperty";
+import UserList from "../../component/OwnerList/OwnerList";
+import "../Dashboard/Dashboard.css";
+import NavSection from "../../Shared/Navbar/NavSection";
+
+const Dashboard = () => {
+  const { user } = useContext(AuthContext);
+  const [isAdmin] = useAdmin(user?.email)
+  const [myProperty, setMyProperty] = useState([]);
+  useEffect(() => {
+    fetch("dashboard-data.json")
+      .then((res) => res.json())
+      .then((data) => setMyProperty(data));
+  }, []);
+  return (
+    <div className="container">
+      <NavSection></NavSection>
+      <h3 className="text-center mb-5 mt-5">Welcome to Dashboard</h3>
+      <div className="row">
+        <div className="col-md-3 col-lg-3 col-sm-12">
+
+          {
+            // isAdmin &&
+            <>
+              <li><Link to="/dashboard/allRenters">All Renters</Link></li>
+              <li><Link to="/dashboard/allOwners">All Owners</Link></li>
+            </>
+          }
+        </div>
+        <div className="col-md-9 col-lg-9  col-sm-12">
+          <Outlet></Outlet>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
