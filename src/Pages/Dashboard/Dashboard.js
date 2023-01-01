7fa6efffda742a7fa84dfa198e381d8fef69d3c0
RaiAn-CSE <68633815+RaiAn-CSE@users.noreply.github.com>
2023-01-01 22:43:04 +0600

    add data

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -4,6 +4,7 @@ import "../Dashboard/Dashboard.css";
 import NavSection from "../../Shared/Navbar/NavSection";
 import useAdmin from "../../hooks/useAdmin";
 import { AuthContext } from "../../contexts/AuthProvider";
+import { Typewriter } from 'react-simple-typewriter'
 
 const Dashboard = () => {
   const { user } = useContext(AuthContext);
@@ -17,12 +18,29 @@ const Dashboard = () => {
   return (
     <div className="container">
       <NavSection></NavSection>
+      <div>
+        <p className='my-4 text-gray-500 dark:text-gray-400'> Hi, This is Robiul Hasan Raian
+          <br />
+          <span style={{ color: 'white', fontWeight: 'bold' }}>
+            {/* Style will be inherited from the parent element */}
+            <Typewriter
+              words={['A passionate frontend developer from Bangladesh', 'A MERN Stack Web Developer also']}
+              loop={5}
+              cursor
+              cursorStyle='_'
+              typeSpeed={70}
+              deleteSpeed={50}
+              delaySpeed={1000}
+            />
+          </span></p>
+
+      </div>
       <h3 className="text-center mb-5 mt-5">Welcome to Dashboard</h3>
       <div className="row">
         <div className="col-md-3 col-lg-3 col-sm-12">
 
           {
-            // isAdmin &&
+            isAdmin &&
             <>
               <li><Link to="/dashboard/allRenters">All Renters</Link></li>
               <li><Link to="/dashboard/allOwners">All Owners</Link></li>
